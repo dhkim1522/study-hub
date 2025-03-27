a6a40e7ac032f444b960c0f0b423cbc9fcbc537d
dhkim1522 <dhkim1522@naver.com>
2025-03-27 17:08:56 +0900

    @TransactionalEventListener, @ApplicationModuleListener 이벤트 리스너 동기, 비동기 실행 여부 테스트

--- src/main/kotlin/com/dhkim/springmodulith/stock/StockService.kt
+++ src/main/kotlin/com/dhkim/springmodulith/stock/StockService.kt
@@ -4,15 +4,20 @@ import com.dhkim.springmodulith.order.OrderCreateEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.modulith.events.ApplicationModuleListener
 import org.springframework.stereotype.Service
+import org.springframework.transaction.event.TransactionalEventListener
+import java.time.LocalDateTime
 
 @Service
 class StockService(
     private val stockRepository: StockRepository,
     private val publisher: ApplicationEventPublisher
 ) {
+//    @TransactionalEventListener
     @ApplicationModuleListener
     fun handle(event: OrderCreateEvent) {
-        println("재고 수정 처리 시작: 주문 번호 ${event.orderId}")
+        Thread.sleep(10000)
+        println("재고 처리 Thread: ${Thread.currentThread().name}")
+        println("재고 수정 처리 시작: 주문 번호 ${event.orderId}, ${LocalDateTime.now()}")
 
         val stock = stockRepository.findByProductId(event.productId)
             ?: throw IllegalArgumentException("상품이 존재 하지 않음")
