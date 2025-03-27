26b4cce30497ff2dc4bd3fd42de4728841a0c0c0
dhkim1522 <dhkim1522@autocrypt.io>
2025-03-27 10:58:20 +0900

    Initial commit

---
+++ src/test/kotlin/com/dhkim/springmodulith/SpringModulithApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dhkim.springmodulith
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringModulithApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
