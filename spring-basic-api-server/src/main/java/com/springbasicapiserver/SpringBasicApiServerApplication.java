78c55c5c983ae9709344220428bf7b448f8e1be3
dhkim1522 <dhkim1522@autocrypt.io>
2025-06-09 16:29:12 +0900

    SpringBoot 기본 API 개발을 위한 프로젝트

---
+++ spring-basic-api-server/src/main/java/com/springbasicapiserver/SpringBasicApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.springbasicapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicApiServerApplication.class, args);
+    }
+
+}
---
+++ spring-basic-api-server/src/test/java/com/springbasicapiserver/SpringBasicApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbasicapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
