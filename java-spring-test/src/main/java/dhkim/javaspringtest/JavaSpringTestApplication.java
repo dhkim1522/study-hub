182e0a9121a72e2d3dd39c8de6f7e445d9f51909
dhkim1522 <dhkim1522@naver.com>
2025-07-11 15:23:16 +0900

    init

---
+++ java-spring-test/src/main/java/dhkim/javaspringtest/JavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package dhkim.javaspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringTestApplication.class, args);
+    }
+
+}
